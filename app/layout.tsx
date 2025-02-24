6e5c2036884dd53b3bd9d3e9c270a22959529bb3
Shardul0x <79738942+Shardul0x@users.noreply.github.com>
2025-02-25 00:36:02 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,31 @@
-export const dynamic = 'force-dynamic';
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-import type { Metadata } from "next";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ 
-  subsets: ["latin"], 
-  variable: '--font-inter' 
-});
-
-const ibmPlexSerif = IBM_Plex_Serif({
-  subsets: ['latin'],
-  weight: ['400, 600 , 700'], // Ensure no leading space here
-  variable: '--font-ibm-plex-serif'
-});
-
-export const metadata: Metadata = {
-  title: "Hisab-Kitab",
-  description: "Simplify Your Finances, Amplify Your Savings!",
-  icons: {
-    icon: '/icons/logo.svg'
-  }
-};
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = await getLoggedInUser();
+
+  if(!loggedIn) redirect('/sign-in')
+
   return (
-    <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
-      </body>
-    </html>
+      </div>
+    </main>
   );
 }
